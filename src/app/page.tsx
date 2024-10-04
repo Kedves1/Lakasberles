1587a801d4ed9d05bc3cf7921f775bd231e8433c
Kedves <93329472+Kedves1@users.noreply.github.com>
2024-10-04 19:37:22 +0200

    FIx: Card box shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home({
           />
         </div>
         <div className="max-w-[1200px] bg-white m-auto h-[600px] my-11 rounded-xl shadow-sm shadow-black/20 max-sm:mx-1"></div>
-        <div className="m-auto max-sm:mx-1 rounded-xl bg-white text-4xl pt-4 max-w-[1800px] pb-10">
+        <div className="m-auto max-sm:mx-1 rounded-xl bg-white text-4xl pt-4 max-w-[1800px] pb-10 shadow-sm shadow-black/20">
           <div className="border-b-[2px] border-b-black/20 mx-10">
             Kedvelt Házak
           </div>
