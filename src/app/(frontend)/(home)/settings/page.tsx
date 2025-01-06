08d9dec08fe4e80d985eb0db66adccde3c0a06ef
Kedves1 <gombarmartin2006@gmail.com>
2025-01-06 08:45:41 +0100

    Feat: Settings page

---
+++ src/app/(frontend)/(home)/settings/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <main className="w-screen h-screen pt-[120px] flex justify-center items-center">
+      <div className="max-w-[1600px] flex w-full h-[70vh] rounded-xl bg-main p-10">
+        <div className="basis-1/5">
+          <div className=""></div>
+        </div>
+        <div className="basis-4/5 border-[2px] border-black/20 rounded-xl"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Settings;
