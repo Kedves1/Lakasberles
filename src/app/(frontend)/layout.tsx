e79268cb0b8a424bf32923bd35b246dadc3e7574
Kedves1 <gombarmartin@pollakszentes.hu>
2025-01-13 08:45:33 +0100

    Feat: housepics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Providers from "./components/Providers";
 import { Toaster } from "./components/ui/toaster";
 import "./global.css";
@@ -8,18 +9,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="hu">
-      <script
+    <>
+      <Script
         defer
         src="https://umami.gemes.eu/script.js"
         data-website-id="923394af-d972-4518-85e7-6ded41da33a9"
       />
-      <body className="font-normal antialiased bg-background min-h-screen w-screen overflow-x-clip text-primary">
-        <Providers>
-          {children}
-          <Toaster />
-        </Providers>
-      </body>
-    </html>
+      <html lang="hu">
+        <body className="font-normal antialiased bg-background min-h-screen w-screen overflow-x-clip text-primary">
+          <Providers>
+            {children}
+            <Toaster />
+          </Providers>
+        </body>
+      </html>
+    </>
   );
 }
